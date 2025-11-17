b8a2a26bc0c5ca6667b6c11da666e5a9b53fa792
qbdoqf <h098s02915@chunchon.gwe.hs.kr>
2025-11-17 12:06:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,9 +77,9 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
 
     labels[0]:{"texts" :["총 맞은 것처럼 가슴이 너무 아파"],
                "videos":["https://www.youtube.com/shorts/_xALyZVSIOo"]},
-    labels[1]:{"texts" :["헛된 꿈을 꾼 자"],
+    labels[1]:{"texts" :["주정뱅이"],
                "videos":["https://www.youtube.com/watch?v=asxPvaWN-qw"]},
-    labels[2]:{"texts" :["그저 돈이 많고 싶었던 사람"],
+    labels[2]:{"texts" :["강도"],
                "videos":["https://www.youtube.com/watch?v=BefZXjQbWMo"]},
     labels[3]:{"texts" :["펑(한강다리 터지는 소리)"],
                "videos":["https://www.youtube.com/watch?v=mi_lQiQk5OA"]},
